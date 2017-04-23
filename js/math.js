d92bcc15252b2ab2fe9d59e0fa830633bf3ddb86
Osipov Andrew <mekagem@gmail.com>
2017-04-23 12:50:03 +0300

    small fixes

--- js/math.js
+++ js/math.js
@@ -1,9 +1,9 @@
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function clamp(value, min, max) {
-  return Math.max(Math.min(value, max), min);
+    return Math.max(Math.min(value, max), min);
 }

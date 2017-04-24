32180285c92f979af9374978e667e228d328c3bb
Osipov Andrew <mekagem@gmail.com>
2017-04-24 21:18:20 +0300

    kamushki and progress baruhky

--- js/building.js
+++ js/building.js
@@ -1,19 +1,28 @@
 var BUILDING_FORT = {name: "Fort", type: Fort};
 
 Building.prototype = Object.create(Unit.prototype);
-function Building(x, y, view, type) {
+function Building(x, y, view, type, buildingTime) {
     Unit.call(this, x, y, view, type);
+    this.onBuildCallback = null;
+    this.buildingTime = buildingTime;
+    this.buildingProgress = 0;
+}
+
+Building.prototype.buildTick = function(world) {
+    if (this.buildingProgress < this.buildingTime) {
+        this.buildingProgress += 1;
+        if (this.buildingProgress == this.buildingTime) {
+            world.cells[this.x][this.y].fortify();
+            world.removeUnit(this);
+        }
+    }
 }
 
 Fort.prototype = Object.create(Building.prototype);
 function Fort(x, y) {
-    var shape = new createjs.Shape();
-    shape.graphics.beginFill("grey");
-    drawTile(shape);
-    var view = shape;
-    // var view = new createjs.Sprite(assets.resourcesSpriteSheet, "tree");
-    // var view = new createjs.Sprite(assets.spriteSheet, "fort");
-    Building.call(this, x, y, view, UNIT_FORT);
+    var view = new createjs.Sprite(assets.resourcesSpriteSheet, "kamushki");
+    view.alpha = 0.5;
+    Building.call(this, x, y, view, UNIT_FORT, 10);
 }
 
 Fort.prototype.requirements = [

30bb4a749b3553f4d3c32c704ec1de4611474e89
derubioo <derubioo@gmail.com>
2017-04-24 21:20:55 +0300

    logging for mouse moves.

--- js/game.js
+++ js/game.js
@@ -271,8 +271,11 @@ function initGame() {
 
 function updateSelectedCell(game) {
     var local = game.world.container.globalToLocal(stage.mouseX, stage.mouseY);
+    console.log("isometric local mouse coordinates: x = " + local.x + ", y = " + local.y);
     var cart = isometricToCartesian(local.x, local.y);
+    console.log("cartesian local mouse coordinates: x = " + cart.x + ", y = " + cart.y);
     var cell = new Point(Math.floor(cart.x / CELL_SIZE), Math.floor(cart.y / CELL_SIZE));
+    console.log("cell coordinates: x = " + cell.x + ", y = " + cell.y);
     if (game.world.cellIsSelectable(cell.x, cell.y)) {
         game.selectedCellPosition = cell;
         var iso = cartesianToIsometric(cell.x * CELL_SIZE, cell.y * CELL_SIZE);
--- js/map.js
+++ js/map.js
@@ -326,6 +326,9 @@ World.prototype.cellIsCutVertex = function(x, y) {
 }
 
 World.prototype.cellIsSelectable = function(x, y) {
+    console.log("World.prototype.cellIsSelectable(x, y): x = " + x + ", y = " + y);
+    console.log("this.cellIsWaterNearLand(x, y) = " + this.cellIsWaterNearLand(x, y));
+    console.log("this.cellIsLand(x, y) = " + this.cellIsLand(x, y));
     return this.cellIsWaterNearLand(x, y) || this.cellIsLand(x, y);
 }
 
@@ -360,13 +363,13 @@ World.prototype.shakeTiles = function() {
 
 World.prototype.updateBorderWater = function() {
     if (this.borderWater) {
-        for (i = 0; i < this.borderWater.length; ++i) {
+        for (var i = 0; i < this.borderWater.length; ++i) {
             var waterCell = new Cell(CELL_TYPE_WATER);
             this.removeShapeFromTilesContainer(waterCell.shape, this.borderWater.x, this.borderWater.y);
         }
     }
     this.borderWater = this.getBorderWater();
-    for (i = 0; i < this.borderWater.length; ++i) {
+    for (var i = 0; i < this.borderWater.length; ++i) {
         var waterCell = new Cell(CELL_TYPE_WATER);
         this.addShapeToTilesContainer(waterCell.shape, this.borderWater.x, this.borderWater.y);
     }
